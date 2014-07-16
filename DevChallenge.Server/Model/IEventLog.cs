20de2cbf093c003f4299f13b53738b922c60adde
Micael <micael.belin@gmail.com>
2014-07-16 22:11:58 +0200

    Added forgotten file

---
+++ DevChallenge.Server/Model/IEventLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevChallenge.Server.Model
+{
+    public interface IEventLog
+    {
+        void Add(Model.EventLogType type,string msg);
+    }
+
+    public enum EventLogType
+    {
+        Message,
+        Error,
+    }
+
+
+}
